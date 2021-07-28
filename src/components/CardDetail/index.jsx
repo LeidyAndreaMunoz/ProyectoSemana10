f1b74b668d77b384c213597e3b8a8218e3902ef4
Joan Rodriguez <joan.rodriguez@alteru.co>
2021-07-27 19:14:31 -0500

    data

--- src/components/CardDetail/index.jsx
+++ src/components/CardDetail/index.jsx
@@ -3,22 +3,22 @@ import './styles.css';
 
 export default function CardDetail() {
 
-    let cartas = GetData("Red Dragon Ninja");
+    let cartas = GetData("Tornado Dragon");
 
-    return (
+    return cartas.length === 0 ? <h1>Loading...</h1> : (
         <section className="detalles-carta">
             <div className="detalle-img">
-                <img src="https://ygoprodeck.com/pics/58165765.jpg" alt="" />
+                <img src={cartas.data[0].card_images[0].image_url} alt="" />
             </div>
             <div className="detalle-text">
-                <h4>Nombre: </h4>
-                <h4>Tipo: </h4>
-                <h4>Ataque: </h4>
-                <h4>Defensa: </h4>
-                <h4>Nivel: </h4>
-                <h4>Raza: </h4>
-                <h4>Atributo: </h4>
-                <h4>Arquetipo: </h4>
+                <h4>Nombre: {cartas.data[0].name}</h4>
+                <h4>Tipo: {cartas.data[0].type}</h4>
+                <h4>Ataque: {cartas.data[0].atk}</h4>
+                <h4>Defensa: {cartas.data[0].def}</h4>
+                <h4>Nivel: {cartas.data[0].level}</h4>
+                <h4>Raza: {cartas.data[0].race}</h4>
+                <h4>Atributo: {cartas.data[0].attribute}</h4>
+                <h4>Arquetipo: {cartas.data[0].archetype}</h4>
             </div>
         </section>
     )
