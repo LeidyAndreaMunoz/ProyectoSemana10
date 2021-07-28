5dcf12fc19a4cb07b201a9fafaa62a5c874549f7
Joan Rodriguez <joan.rodriguez@alteru.co>
2021-07-28 15:50:36 -0500

    rutas de componentes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 import './header.css';
 
@@ -5,7 +6,7 @@ export default function Header() {
     return (
         <header>
             <section className="Header-logo">
-                <a href=" "><img src={logo} alt="Logo" /></a>
+                <Link to ="/home"><img src={logo} alt="Logo" /></Link>
             </section>
             <section className="Header-login">
                 <div>
--- src/views/Homepage/index.jsx
+++ src/views/Homepage/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Carousel from '../../components/Carousel';
-import Search from '../../components/Search';
+import Carousel from '../../Components/Carousel';
+import Search from '../../Components/Search';
 import CardsHome from '../../Components/CardsHome';
 
 function Homepage() {
